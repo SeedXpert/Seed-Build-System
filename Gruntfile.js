eb76f9b5275b56ef18a05e06f181b475168986d3
SEED™ <seed.sequomics@gmail.com>
2016-11-08 20:14:11 +0530

    Initial commit
    --/The Heart of Build + Test System/-- of "Seed-Build-System®" | Build: Örebro

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,31 @@
 #!/usr/bin/env node
+
+
+/*!
+ * Seed-Build-System®
+ * A Xpert Build System — http://seed.sequomics.com/.
+ * ____________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/.
+ * The JavaScript Task Runner.
+ * ____________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ____________________________________________________________________
+ *
+ * @date      : 08-Nov-2016
+ * @license   : Apache License
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-cli
+ * @build     : Seed™: Örebro
+ *              |---- A Sequømics Build Product, http://sequomics.com/.
+ * ____________________________________________________________________
+ *
+ * --/The Heart of Build + Test System/-- of "Seed-Build-System®".
+ * ____________________________________________________________________
+ */
