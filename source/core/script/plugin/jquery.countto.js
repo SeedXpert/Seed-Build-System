98831748b6d216ba080cc169cf80f583d443d638
SEED™ <seed.sequomics@gmail.com>
2016-11-09 00:28:13 +0530

    Initial commit
    jQuery countTo — https://github.com/mhuggins/jquery-countTo

---
+++ source/core/script/plugin/jquery.countto.js
@@ -0,0 +1,8 @@
+/*!
+ * jQuery countTo — v1.2.0
+ * A jQuery plugin that will count up (or down) to a target number at a specified speed.
+ * https://github.com/mhuggins/jquery-countTo
+ *
+ * Copyright 2016, Matt Huggins.
+ * Released under the MIT License.
+ */
