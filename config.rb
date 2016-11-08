fd6e7f5a74d141fc90db091ba5284bf4ed40f83a
SEED™ <seed.sequomics@gmail.com>
2016-11-08 20:26:11 +0530

    Initial commit
    Seed-Build-System® | Build: Örebro
    Help: pass the --sourcemap compiler option for future debug.

--- config.rb
+++ config.rb
@@ -1,1 +1,36 @@
 #!/usr/bin/env ruby
+
+
+# Seed-Build-System®
+# -----------------------------------------
+# From the Desk of Sequømics Corporation.
+# -----------------------------------------
+#     	   With ♥ by Prabhat Kumar.
+# -----------------------------------------
+# @author     : Prabhat Kumar.
+# @copyright  : Sequømics Corporation.
+# @homepage   : http://sequomics.com/.
+# @require    : Ruby
+# @require    : Sass
+# @require    : Compass
+# @version    : 1.0.0
+# @build      : Örebro
+# @date       : 08-Nov-2016
+# @license    : Apache, version 2.0
+# -----------------------------------------
+# Principle   : Standard Module Definition
+# -----------------------------------------
+
+# -------------------------------------- //
+#         Using @mixin directive         //
+#                  ~   ~                 //
+#                 (.) (.)                //
+#                    -                   //
+#                  `\^/`                 //
+#        @author : Prabhat Kumar         //
+#          @date : 08-Nov-2016           //
+# -------------------------------------- //
+
+
+# Sass - (3.4.21)     : Selective Steve
+# Compass - (v1.0.3)  : Polaris
