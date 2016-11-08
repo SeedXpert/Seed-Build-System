0104e7bd62ba6f1cd5a413c54cdcbc3078d89dd9
SEED™ <seed.sequomics@gmail.com>
2016-11-08 23:14:30 +0530

    Initial commit
    The Non-AMD JavaScript — for defining "root" configurations.

---
+++ source/core/script/root.es.js
@@ -0,0 +1,24 @@
+/*!
+ * Seed-Build-System®
+ * _______________________________________________________________
+ *
+ * The Non-AMD JavaScript — for defining "root" configurations.
+ * _______________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * _______________________________________________________________
+ *
+ * @date      : 08-Nov-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : .eslintrc
+ * _______________________________________________________________
+ *
+ * --/The Heart of Non-AMD System/-- of "Seed-Build-System®".
+ * _______________________________________________________________
+ */
