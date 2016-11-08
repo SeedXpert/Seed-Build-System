49a9b487df3cb0c0a130803f655d9a67e04b6998
SEED™ <seed.sequomics@gmail.com>
2016-11-08 18:55:11 +0530

    Initial commit
    install.sh, shell script to --/install/-- whole dependencies of "Seed-Build-System®" in one go.
    Usage: $ sh install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+# =========================== I'm Prabhat® ===========================
+#                Technical Architect & Data Scientist
+#                          Seed-Build-System®
+#                          ------------------
+#       Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+# Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+# ====================================================================
