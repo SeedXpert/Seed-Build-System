d8b557003ee3305813a006ff42b2d5f3a47cc848
SEED™ <seed.sequomics@gmail.com>
2016-11-09 00:31:41 +0530

    Initial commit
    textillate.js — https://github.com/jschr/textillate

---
+++ source/core/script/plugin/jquery.textillate.js
@@ -0,0 +1,8 @@
+/*!
+ * textillate.js — v0.4.0
+ * A simple plugin for CSS3 text animations.
+ * https://github.com/jschr/textillate
+ *
+ * Copyright 2012 - 2013, Jordan Schroter.
+ * Released under the MIT License.
+ */
