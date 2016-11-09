9b9ce19458f2ef7434e6641a4e827c45ecfeb782
SEED™ <SeedXpert@users.noreply.github.com>
2016-11-09 16:36:09 +0530

    Initial commit
    Architecture — Architecture of SeedXpert™.

---
+++ source/core/script/module/architecture.js
@@ -0,0 +1,12 @@
+/*!
+ * architecture — Architecture of SeedXpert™ — v1.0.0
+ * Build      : Örebro
+ * Mode       : -//-Development-//-
+ * --------------------------------------------------------------------
+ * @architecture    : Prabhat Kumar, http://prabhatkumar.org/
+ * @author          : Prabhat Kumar, https://github.com/iamprabhat
+ * --------------------------------------------------------------------
+ * Copyright © 2014 - 2016, Sequømics Corporation, All Rights Reserved.
+ * Licensed under Apache.
+ * --------------------------------------------------------------------
+ */
