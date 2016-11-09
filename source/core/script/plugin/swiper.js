7dba90925a20703dc221a729e8fca590bfde6360
SEED™ <SeedXpert@users.noreply.github.com>
2016-11-09 16:01:12 +0530

    Initial commit
    Swiper — https://github.com/nolimits4web/Swiper

---
+++ source/core/script/plugin/swiper.js
@@ -0,0 +1,12 @@
+/*!
+ * Swiper — v3.4.0
+ * Most modern mobile touch slider and framework with hardware accelerated transitions.
+ * http://www.idangero.us/swiper/
+ * 
+ * Copyright 2016, Vladimir Kharlampidi.
+ * http://www.idangero.us/
+ * 
+ * Licensed under MIT
+ * 
+ * Released on: October 16, 2016
+ */
