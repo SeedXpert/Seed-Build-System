6e7838c53d515e4ee3bfcce1f9ef9114b1a24ccd
SEED™ <SeedXpert@users.noreply.github.com>
2016-11-09 16:06:48 +0530

    Initial commit
    Lettering — https://github.com/davatron5000/Lettering.js

---
+++ source/core/script/plugin/jquery.lettering.js
@@ -0,0 +1,6 @@
+/*!
+ * Lettering — v0.7.0
+ * Lettering.js, a jQuery plugin for radical Web Typography.
+ * Copyright 2010, Dave Rupert http://daverupert.com/.
+ * Released under the WTFPL license.
+ */
