fa20b75ed6b0fe590d0befe4d556349ef65721a2
SEED™ <SeedXpert@users.noreply.github.com>
2016-11-09 16:40:32 +0530

    Initial commit
    The Non-AMD JavaScript - for defining "Krzywinski's Hive Plot".

---
+++ source/core/script/engine/hiveplot.js
@@ -0,0 +1,16 @@
+/*!
+ * Seed-Build-System®
+ * _______________________________________________________________
+ * The Non-AMD JavaScript - for defining "Krzywinski's Hive Plot".
+ * _______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * _______________________________________________________________
+ * @date      : 09-Nov-2016
+ * @license   : Apache, version 2.0
+ * @require   : d3.js®
+ * @require   : Modernizr
+ * _______________________________________________________________
+ */
