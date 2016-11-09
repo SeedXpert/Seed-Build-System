57bd4b4f133a8f53cafd95d3969fc77644d0ae11
SEED™ <SeedXpert@users.noreply.github.com>
2016-11-09 16:32:10 +0530

    Initial commit
    Scrolling — Smooth Scrolling.

---
+++ source/core/script/module/scrolling.js
@@ -0,0 +1,12 @@
+/*!
+ * scrolling — Smooth Scrolling — v1.0.0
+ * Build      : Örebro
+ * Mode       : -//-Development-//-
+ * --------------------------------------------------------------------
+ * @architecture    : Prabhat Kumar, http://prabhatkumar.org/
+ * @author          : Prabhat Kumar, https://github.com/iamprabhat
+ * --------------------------------------------------------------------
+ * Copyright © 2014 - 2016, Sequømics Corporation, All Rights Reserved.
+ * Licensed under Apache.
+ * --------------------------------------------------------------------
+ */
